d8fbc16c0b9634312988257ef7ad84d6d574b4ea
alotfy2019 <elshenawy19@gmail.com>
2021-02-13 20:25:39 +0200

    lesson 4 dom course

--- app.js
+++ app.js
@@ -3,20 +3,55 @@
 //     navToggle.classList.toggle('close')
 // }
 
-//Dom Course Start //
+///////////////////      Dom Course Start      ////////////////////
+
 // var myP = document.createElement('p')
 
 //     myPText = document.createTextNode('this paragraph created with javascript')
-    
+
 //     myP.appendChild(myPText)
 
 //     document.body.appendChild(myP)
-// Find Dom Elements //
-[1] 
+
+///////////////////     Find Dom Elements       ////////////////////
+
+// [1] - document.getElementById()
+// [2] - document.getElementsByTagName()
+// [3] - document.getElementsByClassName()  // Not Compitable With IE 8
+// [4] - document.querySelectorAll()        // Not Compitable With IE 8
+
+// var myDiv = document.getElementById('test');
+// myDiv.innerHTML = 'Changes By Javascript';
+
+// var myElement = document.getElementsByTagName('div');
+// myElement[1].innerHTML = 'Changed By Javascript'; // Will Not Change 
+
+// var myElement = document.getElementsByClassName('test');
+// myElement[0].innerHTML = 'Changed By Javascript';
+
+// var myElement = document.querySelectorAll('div.test'); //tag and class 
+// myElement[0].innerHTML = 'Changed By Javascript';
+
+///////////////////    Find Element By Objects    /////////////////////
+//  document.title
+//  document.images its object then access it with index     document.images[0].src
+//  document.forms
+
+// var myDiv = document.getElementById('show')
+// myDiv.innerHTML = document.title
+// myDiv.innerHTML = document.images[1].src + document.images[2. src]
+// for (i = 0; i < document.images.length; i++) {
+//     document.write(document.images[i].src)
+// }
+var myDiv = document.getElementById('show');
+
+// myDiv.innerText = document.forms[0].x2.value
+// myDiv.innerText = document.forms[0].x2.type
+// myDiv.innerText = document.forms[0].x2.email
 
 
+///////////////////    Dom Course END    /////////////////////
 
-// Dom Course End //
 
 // Bom Course Start //
 
