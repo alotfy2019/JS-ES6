7e3106134f0f57639a2700bc8b5aa312b87fa976
alotfy2019 <elshenawy19@gmail.com>
2021-02-13 14:44:15 +0200

    bom lessons learned

---
+++ app.js
@@ -0,0 +1,44 @@
+function myFunction() {
+    var navToggle = document.getElementById('nav-links')
+    navToggle.classList.toggle('close')
+}
+
+
+// document.cookie("name=value, expires=Date, path=")
+// document.cookie = 'favfont=Droid,';
+
+// Screen
+// console.log('Screen Width Is: ' + screen.width)
+// console.log('Screen Width Is: ' + screen.height)
+// console.log('Screen Available Width Is: ' + screen.availWidth)
+// console.log('Screen Available Width Is: ' + screen.availHeight)
+// console.log('Screen Color Depth Is :' + screen.colorDepth)
+
+// // window.navigator
+// console.log(navigator.appCodeName)
+// console.log(navigator.appName)
+// console.log(navigator.appVersion)
+// console.log(navigator.platform)
+// console.log(navigator.cookieEnabled)
+// console.log(navigator.language)
+// console.log(navigator.onLine)
+// console.log(navigator.geolocation)
+
+// open
+//window.open(Url, Name Or Attribute, Specification, History Replace)
+// document.getElementById('open').onclick = function() {
+//     window.open("https://www.google.com", "_blank", "", "")
+// }
+
+// count down timer
+// var myDiv = document.getElementById("count")
+
+// function countDown() {
+//     if (myDiv.textContent <= 0) {
+//         myDiv.textContent = 'Done...';
+//         clearInterval(myTime)
+//     } else {
+//         myDiv.textContent -= 1
+//     }
+// }
+// var myTime = setInterval(countDown, 1000);,
